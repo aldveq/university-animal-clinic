c45f6cad542c26757209e9691517f16e01a90913
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2022-12-05 21:17:51 -0600

    cleaning up unnecessary code

--- wp-content/themes/university-animal-clinic/functions.php
+++ wp-content/themes/university-animal-clinic/functions.php
@@ -21,31 +21,3 @@ function university_animal_clinic_get_theme_instance() {
 	\UniversityAnimalClinic\Inc\University_Animal_Clinic_Theme::get_instance();
 }
 university_animal_clinic_get_theme_instance();
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
--- wp-content/themes/university-animal-clinic/template-parts/content.php
+++ wp-content/themes/university-animal-clinic/template-parts/content.php
@@ -17,19 +17,8 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				university_animal_clinic_posted_on();
-				university_animal_clinic_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+		?>
 	</header><!-- .entry-header -->
-
-	<?php university_animal_clinic_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<?php
@@ -56,8 +45,4 @@
 		);
 		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php university_animal_clinic_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
