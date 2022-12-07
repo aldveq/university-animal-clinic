15f18fabd9b9fcfc3fe08e0766476ec49c68b203
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2022-12-07 17:17:02 -0600

    added header theme option fields to header template

--- wp-content/themes/university-animal-clinic/header.php
+++ wp-content/themes/university-animal-clinic/header.php
@@ -9,6 +9,18 @@
  * @package University_Animal_Clinic
  */
 
+ // Disclaimer Data
+ $top_bar_disclaimer_text = get_field('top_bar_disclaimer_text', 'option');
+ $top_bar_disclaimer_cta = get_field('top_bar_disclaimer_cta', 'option');
+
+ // Phone Data
+ $top_bar_phone_label = get_field('top_bar_phone_label', 'option');
+ $top_bar_phone_number = get_field('top_bar_phone_number', 'option');
+
+ // Primary & Secondary CTA Data
+$top_bar_primary_cta = get_field('top_bar_primary_cta', 'option');
+$top_bar_secondary_cta = get_field('top_bar_secondary_cta', 'option');
+
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
@@ -25,32 +37,72 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'university-animal-clinic' ); ?></a>
 
-	<div class="covid-message">
-		<div class="container">
-			<div class="covid-text">Covid-19 message: University Animal Clinic is always looking out for the safety of
-				our clients and staff.
-				Please visit our COVID-19 Updates page for more information on our curbside
-				check-in process.</div>
-			<div class="covid-link">
-				<a class="learn-more" href="#">learn more <span class="icon-arrow-right"></span></a>
+	<?php
+		if(!empty($top_bar_disclaimer_text) && !empty($top_bar_disclaimer_cta)):
+			$top_bar_disclaimer_cta_url = $top_bar_disclaimer_cta['url'];
+			$top_bar_disclaimer_cta_title = $top_bar_disclaimer_cta['title'];
+			$top_bar_disclaimer_cta_target = $top_bar_disclaimer_cta['target'] ? '_blank' : '_self';
+			?>
+			<div class="covid-message">
+				<div class="container">
+					<div class="covid-text"><?php echo $top_bar_disclaimer_text; ?></div>
+					<div class="covid-link">
+						<a class="learn-more" href="<?php echo esc_url($top_bar_disclaimer_cta_url); ?>" target="<?php echo esc_attr($top_bar_disclaimer_cta_target); ?>"><?php echo esc_html($top_bar_disclaimer_cta_title); ?> <span class="icon-arrow-right"></span></a>
+					</div>
+					<a href="javascript:void(0);" class="icon-close removeit"></a>
+				</div>
 			</div>
-			<a href="javascript:void(0);" class="icon-close removeit"></a>
-		</div>
-	</div>
+			<?php
+		endif;
+	?>
+	
 	<header class="header">
 		<div class="container">
 			<div class="header-wrap">
-				<div class="header-logo">
-					<a href="#">
-						<img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/logo.png" alt="University Animal Clinic" />
-					</a>
-				</div>
+				<?php 
+					$custom_logo_id = get_theme_mod( 'custom_logo' );
+					
+					if(!empty($custom_logo_id)):
+						$custom_logo_url = wp_get_attachment_image_url( $custom_logo_id , 'full' );		
+						$custom_logo_alt = get_post_meta($custom_logo_id, '_wp_attachment_image_alt', TRUE);
+						?>
+						<div class="header-logo">
+							<a href="<?php echo esc_url(home_url()); ?>">
+								<img src="<?php echo esc_url($custom_logo_url); ?>" alt="<?php echo esc_attr($custom_logo_alt); ?>" />
+							</a>
+						</div>
+						<?php
+					endif;
+				?>
 				<div class="header-right">
 					<div class="header-content">
-						<div class="call-us"><span>call us today</span> <a href="tel:9413557707">941-355-7707</a></div>
+						<?php
+							if(!empty($top_bar_phone_label) && !empty($top_bar_phone_number)):
+								?>
+								<div class="call-us"><span><?php echo esc_html($top_bar_phone_label); ?></span> <a href="tel:<?php echo esc_attr($top_bar_phone_number); ?>"><?php echo esc_html($top_bar_phone_number); ?></a></div>
+								<?php
+							endif;
+						?>
 						<div class="online-request">
-							<a href="#" class="btn btn-secondary">Online Pharmacy</a>
-							<a href="#" class="btn btn-primary">Request an Appointment</a>
+							<?php
+								if(!empty($top_bar_primary_cta)):
+									$top_bar_primary_cta_url = $top_bar_primary_cta['url'];
+									$top_bar_primary_cta_title = $top_bar_primary_cta['title'];
+									$top_bar_primary_cta_target = $top_bar_primary_cta['target'] ? '_blank' : '_self';
+									?>
+									<a href="<?php echo esc_url($top_bar_primary_cta_url); ?>" target="<?php echo esc_attr($top_bar_primary_cta_target); ?>" class="btn btn-secondary"><?php echo esc_html($top_bar_primary_cta_title); ?></a>
+									<?php
+								endif;
+				
+								if(!empty($top_bar_secondary_cta)):
+									$top_bar_secondary_cta_url = $top_bar_secondary_cta['url'];
+									$top_bar_secondary_cta_title = $top_bar_secondary_cta['title'];
+									$top_bar_secondary_cta_target = $top_bar_secondary_cta['target'] ? '_blank' : '_self';
+									?>
+									<a href="<?php echo esc_url($top_bar_secondary_cta_url); ?>" target="<?php echo esc_attr($top_bar_secondary_cta_target); ?>" class="btn btn-primary"><?php echo esc_html($top_bar_secondary_cta_title); ?></a>
+									<?php
+								endif;
+							?>
 						</div>
 					</div>
 					<div class="header-bottom">
@@ -87,9 +139,15 @@
 		</div>
 
 		<div class="mobile-bottom-header">
-			<div class="call-us">
-				<span>call us </span>
-				<a href="tel:9413557707">941-355-7707</a>
-			</div>
+		<?php
+			if(!empty($top_bar_phone_label) && !empty($top_bar_phone_number)):
+				?>
+				<div class="call-us">
+					<span><?php echo esc_html($top_bar_phone_label); ?></span>
+					<a href="tel:<?php echo esc_attr($top_bar_phone_number); ?>"><?php echo esc_html($top_bar_phone_number); ?></a>
+				</div>
+				<?php
+			endif;
+		?>
 		</div>
 	</header>
